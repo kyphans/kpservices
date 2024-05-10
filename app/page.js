b922aef5056fee1399cd63ed6a0ec94b6445a976
Ky Phan <kyphanz1@gmail.com>
2024-05-10 13:55:25 +0700

    Update homepage

--- app/page.js
+++ app/page.js
@@ -4,13 +4,18 @@ export default function Home() {
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center gap-4'>
       <div>
-        <span className='text-2xl bold'>KP API Services</span>
+        <span className='text-3xl font-bold'>KP API Services</span>
       </div>
       <div className='route'>
+        <span className='font-bold'>
+          {'[GET] ShopeeFood API: /api/food/{restaurant_name} '}
+        </span>
         <Link
           className='text-blue-700'
           href='/api/food/starbucks-coffee-lakai-building'>
-          {'Example Shopeefood API: /api/food/starbucks-coffee-lakai-building'}
+          {
+            'Example "starbucks-coffee-lakai-building"'
+          }
         </Link>
       </div>
     </div>
